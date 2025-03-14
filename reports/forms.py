82150ecd62147ce161f6699ed7901b27a353e027
dmolanor <d.molanor@uniandes.edu.co>
2025-03-13 22:08:46 -0500

    Fix db errors and add Jmeter tests

--- reports/forms.py
+++ reports/forms.py
@@ -5,23 +5,23 @@ class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
         fields = [
-            'id',
-            'namw',
+            #'report_id',
+            'name',
             'patient_id',
             'patient_name',
             'summary',
             'image_url',
             'result',
-            'dateTime',
+            'created_at',
         ]
 
         labels = {
-            'id': 'Id',
+            #'report_id': 'Report Id',
             'name': 'Name',
             'patient_id': 'Patient ID',
             'patient_name': 'Patient Name',
             'summary': 'Summary',
             'image_url': 'Image URL',
             'result': 'Result',
-            'dateTime': 'Date Time',
+            'created_at': 'Created At',
         }
--- reports/models.py
+++ reports/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Report(models.Model):
+    #report_id = models.AutoField(primary_key=True)    
     name = models.CharField(max_length=50)
+    patient_id = models.CharField(max_length=20)  # Ajusta max_length según tus necesidades
+    patient_name = models.CharField(max_length=100)
+    summary = models.TextField(blank=True, null=True)  # Permite textos largos, opcional
+    image_url = models.URLField(blank=True, null=True)   # URL de la imagen, opcional
+    result = models.CharField(max_length=50, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)    # Se asigna la fecha y hora de creación
 
     def __str__(self):
-        return '{}'.format(self.name)
-
+        return self.name
